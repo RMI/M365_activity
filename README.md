491be348dc5761308834b2cb522f4b062110c2aa
glnnhoffman <ghoffman@rmi.org>
2023-07-14 09:37:39 -0600

    updating file paths

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # M365_activity
+
+## Purpose: Extract Power BI Activity Log data weekly to construct a historical database
  
--- pbi_activity_wrangle.py
+++ pbi_activity_wrangle.py
@@ -15,7 +15,7 @@ load_dotenv('cred.env')
 rmi_db = os.getenv('DBASE_PWD')
 rmi_db_ip = os.getenv('DBASE_IP')
 
-mydir = Path("pbi_activity_data/")
+mydir = Path("M365_activity/pbi_activity_data/")
 dfs = []
 
 df_base = pd.DataFrame(columns= ['Id', 'CreationTime', 'Operation', 'UserType', 'UserId','Activity', 'ItemName', 'WorkSpaceName', 'DatasetName',
@@ -34,15 +34,15 @@ df = pd.DataFrame(df)
 df1 = pd.concat([df_base, df], ignore_index=True)
 
 # Move new JSONs to archive
-sourcepath='pbi_activity_data/'
+sourcepath='M365_activity/pbi_activity_data/'
 sourcefiles = os.listdir(sourcepath)
-destinationpath = 'pbi_activity_data/archive/'
+destinationpath = 'M365_activity/pbi_activity_data/archive/'
 for file in sourcefiles:
     if file.endswith('.json'):
         shutil.move(os.path.join(sourcepath,file), os.path.join(destinationpath,file))
 
 # Export Excel backup
-name = 'pbi_activity_data/imports/raw_'+ str(date.today()) + '.xlsx'
+name = 'M365_activity/pbi_activity_data/imports/raw_'+ str(date.today()) + '.xlsx'
 df1.to_excel(name)
 
 # Format data for database import
@@ -73,7 +73,7 @@ df_import = df_import.drop(df_import[df_import.Id.isin(df1['Id'])].index.tolist(
 df_import.reset_index(inplace=True)
 df_import = df_import.drop("index", axis= 1)
 
-name = 'pbi_activity_data/imports/import_'+ str(date.today()) + '.xlsx'
+name = 'M365_activity/pbi_activity_data/imports/import_'+ str(date.today()) + '.xlsx'
 df_import['CreationTime'] = df_import['CreationTime'].dt.tz_localize(None)
 
 df_import.to_excel(name)
